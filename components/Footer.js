dd9413bfdace12d0dfaccaecdb102e960bbe4e01
alain-kubwayo <ahkubwayo@gmail.com>
2022-10-23 22:11:23 +0200

    initial

---
+++ components/Footer.js
@@ -0,0 +1,16 @@
+const Footer = () => {
+  return (
+    <div className="footer-section">
+      <footer>
+        <p>UniFinder</p>
+        <ul>
+          <li>&copy; 2022</li>
+          <li>Terms of Use</li>
+          <li>Privacy</li>
+        </ul>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ components/Navbar.js
@@ -0,0 +1,12 @@
+const Navbar = () => {
+  return (
+    <nav>
+      <h1>UniFinder</h1>
+      <ul>
+        <li>All Universities</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
